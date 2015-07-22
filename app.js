162518af9b4ad76c92a0e4e5c08fb6bcfb14316a
Rolando <rolando.jr.rodriguez@gmail.com>
2015-07-22 11:24:20 -0700

    Localhost is working

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var silent = true;
 
-var express = require("express");
-var bodyParser = require("body-parser");
-var errorHandler = require("errorhandler");
+var express = require('express');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
 var mongoose = require('mongoose');
-var express-session = require('express-session')
+var session = require('express-session')
 
 var app = express();
-var root = __dirname + "/public/views";
+var root = __dirname + '/public/';
 
 
 
@@ -19,40 +19,46 @@ var root = __dirname + "/public/views";
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // Simple logger
-app.use(function(req, res, next){
-  console.log("%s %s", req.method, req.url);
-  console.log(req.body);
-  next();
-});
+// app.use(function(req, res, next){
+//   console.log("%s %s", req.method, req.url);
+//   console.log(req.body);
+//   next();
+// });
 
 // Error handler
-app.use(errorHandler({
-  dumpExceptions: true,
-  showStack: true
-}));
-
-app.get("/", function (req, res) {
-	res.render('views/pusher');
-});
+// app.use(errorHandler({
+//   dumpExceptions: true,
+//   showStack: true
+// }));
 
 // Serve static files from directory
-app.use(express.static(root));
+app.use(express.static(__dirname + '/public/'));
 
+// root route (serves index.html)
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/views/index.html');
+});
 //--------------------------
 //SET UP MONGOOSE
 //--------------------------
 
-mongoose.connect('mongodb://localhost/project1');
+mongoose.connect(
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/YOUR_LOCAL_DATABASE_NAME' // plug in the db name you've been using
+);
 
 // Open server on specified port
-if (!silent) console.log("Starting Express server");
-app.listen(process.env.PORT || 3000);
+// if (!silent) console.log("Starting Express server");
+app.listen(process.env.PORT || 3000, function() {
+  console.log('server started!')
+});
 
 
 // Capture uncaught errors
-process.on("uncaughtException", function(err) {
-  console.log(err);
-});
+// process.on("uncaughtException", function(err) {
+//   console.log(err);
+// });
